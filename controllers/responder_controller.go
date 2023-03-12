bd069902dc35ae426e4d767e7de433c723517901
Gergely Brautigam <182850+Skarlso@users.noreply.github.com>
2023-03-12 16:57:25 +0100

    refined the roles

--- controllers/responder_controller.go
+++ controllers/responder_controller.go
@@ -40,7 +40,6 @@ type ResponderReconciler struct {
 //+kubebuilder:rbac:groups=delivery.controller-e2e-framework,resources=responders,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=delivery.controller-e2e-framework,resources=responders/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=delivery.controller-e2e-framework,resources=responders/finalizers,verbs=update
-//+kubebuilder:rbac:groups=delivery.controller-e2e-framework,resources=controller,verbs=get;list;watch;create;update;patch;delete
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
